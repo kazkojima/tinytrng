74f0fde122ca8f8d0f31d0210166d014cf2705fd
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2019-06-22 19:36:06 +0900

    Add module metastable.

--- tinytrng.v
+++ tinytrng.v
@@ -17,6 +17,65 @@
  *
  */
 
+module metastable
+  (
+   input s_in,
+   input r_in,
+   output q,
+   output qb
+   );
+
+   wire       dla_out, dla_in;
+   wire       dlb_out, dlb_in;
+   assign dla_in = s_in;
+   assign dlb_in = r_in;
+
+   wire       lnay, lnaa, lnab;
+   wire       lnby, lnba, lnab;
+   assign q = lnay;
+   assign qb = lnby;
+   assign lnaa = qb;
+   assign lnba = q;
+   assign lnab = dla_out;
+   assign lnbb = dlb_out;
+
+   SB_LUT4 #(.LUT_INIT(16'd7))
+   lna (
+	.O(lnay),
+	.I0(lnaa),
+	.I1(lnab),
+	.I2(1'b0),
+	.I3(1'b0)
+	);
+   
+   SB_LUT4 #(.LUT_INIT(16'd7))
+   lnb (
+	.O(lnby),
+	.I0(lnba),
+	.I1(lnbb),
+	.I2(1'b0),
+	.I3(1'b0)
+	);
+
+   SB_LUT4 #(.LUT_INIT(16'b10101010_10101010))
+   dla (
+	.O(dla_out),
+	.I0(dla_in),
+	.I1(1'b0),
+	.I2(1'b0),
+	.I3(1'b0)
+	);
+
+   SB_LUT4 #(.LUT_INIT(16'b10101010_10101010))
+   dlb (
+	.O(dlb_out),
+	.I0(dlb_in),
+	.I1(1'b1),
+	.I2(1'b1),
+	.I3(1'b0)
+	);
+endmodule // metastable
+
 module tinytrng #(parameter integer NUM_UNITS = 1,
 		  parameter integer XCLK_DIV = 8)
    (
@@ -32,71 +91,11 @@ module tinytrng #(parameter integer NUM_UNITS = 1,
    reg [2:0]  xclk_cycle;
    reg 	      xclk;
    reg 	      ff1, ff2, ff3;
-   reg 	      q, qb;
+   wire       rsffq, rsffqb;
 
-   assign random = lnay;
+   assign random = rsffq;
    assign pulse = clk;
-   assign tp = clk;
-
-
-   wire       lnay, lnaa, lnab;
-   wire       lnby, lnba, lnab;
-   assign q = lnay;
-   assign qb = lnby;
-   assign lnaa = qb;
-   assign lnba = q;
-   assign lnab = dla_out;
-   assign lnbb = dlb_out;
-
-   SB_LUT4 #(
-	     .LUT_INIT(16'd7)
-	     )
-   lna (
-	.O(lnay),
-	.I0(lnaa),
-	.I1(lnab),
-	.I2(1'b0),
-	.I3(1'b0)
-	);
-   
-   SB_LUT4 #(
-	     .LUT_INIT(16'd7)
-	     )
-   lnb (
-	.O(lnby),
-	.I0(lnba),
-	.I1(lnbb),
-	.I2(1'b0),
-	.I3(1'b0)
-	);
-
-   wire       dla_out, dla_in;
-   wire       dlb_out, dlb_in;
-
-   assign dla_in = ff1;
-   assign dlb_in = ff2;
-
-   SB_LUT4 #(
-	     .LUT_INIT(16'b10101010_10101010)
-	     )
-   dla (
-	.O(dla_out),
-	.I0(dla_in),
-	.I1(1'b0),
-	.I2(1'b0),
-	.I3(1'b0)
-	);
-
-   SB_LUT4 #(
-	     .LUT_INIT(16'b10101010_10101010)
-	     )
-   dlb (
-	.O(dlb_out),
-	.I0(dlb_in),
-	.I1(1'b0),
-	.I2(1'b0),
-	.I3(1'b0)
-	);
+   assign tp = ff3;
 
    always @(posedge clk) begin
       if (!resetn)
@@ -120,7 +119,15 @@ module tinytrng #(parameter integer NUM_UNITS = 1,
 	end // else: !if(!resetn)
       ff1 <= xclk;
       ff2 <= xclk;
-      ff3 <= lnba;
+      ff3 <= rsffq;
    end // always @ (posedge clk)
+
+   metastable
+     rsff (
+	   .s_in	(ff1	),
+	   .r_in	(ff2	),
+	   .q		(rsffq	),
+	   .qb		(rsffqb	)
+	   );
 
 endmodule // tinytrng
